d136c5a2f9af910537a72af74c9a7188d5a42ef1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-04-05 13:36:59 +0900

    Bump version to 0.3.0

--- dist/cape.js
+++ dist/cape.js
@@ -14,30 +14,24 @@
       return this.elements[0];
     },
     contentTag: function(tagName) {
-      var options, content, callback, vb;
+      var args, options, content, callback, builder;
 
-      if (typeof arguments[1] === 'string') {
-        content = arguments[1];
-        options = arguments[2] || {};
+      args = Array.prototype.slice.call(arguments, 1);
+      content = extractContent(args);
+      options = extractOptions(args);
+      callback = extractCallback(args);
+
+      if (callback) {
+        builder = new VdomBuilder(this.component, this.formId);
+        if (callback.length === 0) { throw new Error("Callback requires an argument.") }
+        callback.call(this.component, builder);
         attributes = generateAttributes(options);
-        this.elements.push(this.h(tagName, attributes, content));
+        this.elements.push(this.h(tagName, attributes, builder.elements));
       }
       else {
-        if (typeof arguments[1] === 'object') {
-          options = arguments[1];
-          callback = arguments[2];
-        }
-        else {
-          options = {};
-          callback = arguments[1];
-        }
-        if (typeof callback === 'function') {
-          var vb = new VdomBuilder(this.component, this.formId);
-          if (callback.length === 0) { throw new Error("Callback requires an argument.") }
-          callback.call(this.component, vb);
-          attributes = generateAttributes(options);
-          this.elements.push(this.h(tagName, attributes, vb.elements));
-        }
+        content = content || '';
+        attributes = generateAttributes(options);
+        this.elements.push(this.h(tagName, attributes, content));
       }
     },
     tag: function(tagName, options) {
@@ -51,19 +45,23 @@
       this.elements.push(' ');
     },
     form: function(name, options, callback) {
-      var vb, attributes;
+      var builder, attributes;
 
       if (callback.length === 0) { throw new Error("Callback requires an argument.") }
-      if (!this.component.forms[name]) this.component.forms[name] = {};
-      vb = new VdomBuilder(this.component, name);
-      callback.call(this.component, vb);
+      builder = new VdomBuilder(this.component, name);
+      callback.call(this.component, builder);
       options = options || {};
       options['name'] = name;
       if (options['onsubmit'] === undefined) {
         options['onsubmit'] = function(e) { return false };
       }
       attributes = generateAttributes(options);
-      this.elements.push(this.h('form', attributes, vb.elements));
+      this.elements.push(this.h('form', attributes, builder.elements));
+    },
+    labelFor: function(id, content, options) {
+      options = options || {};
+      options['htmlFor'] = id;
+      this.contentTag('label', content, options)
     },
     input: function(options) {
       var form, name, value;
@@ -71,11 +69,8 @@
       options = options || {};
       name = options['name'];
       value = options['value'];
-      if (value === undefined && name !== undefined && this.formId !== undefined) {
-        form = this.component.forms[this.formId];
-        if (form !== undefined && form[name] !== undefined)
-          options['value'] = form[name];
-      }
+      if (value === undefined && name !== undefined && this.formId !== undefined)
+        options['value'] = this.component.getValue(this.formId, name);
       attributes = generateAttributes(options);
       this.elements.push(this.h('input', attributes));
     },
@@ -85,14 +80,11 @@
       options = options || {};
       options['type'] = 'text';
       options['name'] = name;
-      if (options['onkeyup'] === undefined)
-        options['onkeyup'] = function(e) {
-          self.component.forms[self.formId][name] = e.target.value;
-          self.component.refresh();
-        };
       this.input(options);
     },
-    checkBox: function(name, checked, options) {
+    checkBox: function(name, options) {
+      var checked = this.component.getValue(this.formId, name);
+
       options = options || {};
       options['type'] = 'checkbox';
       if (name) options['name'] = name;
@@ -101,19 +93,37 @@
       this.input(options);
     },
     value: function(name) {
-      var form;
-
-      if (name !== undefined && this.formId !== undefined) {
-        form = this.component.forms[this.formId];
-        if (form !== undefined) return form[name];
-      }
+      this.component.getValue(this.formId, name);
     }
   });
 
+  function extractContent(args) {
+    if (typeof args[0] === 'string') return args[0];
+  }
+
+  function extractOptions(args) {
+    for (var i = 0; i < args.length; i++)
+      if (typeof args[i] === 'object') return args[i];
+  }
+
+  function extractCallback(args) {
+    for (var i = 0; i < args.length; i++)
+      if (typeof args[i] === 'function') return args[i];
+  }
+
   function generateAttributes(options) {
+    options = options || {};
     if ('visible' in options && !options['visible']) {
       options['style'] = options['style'] || {};
       options['style']['display'] = 'none';
+    }
+    if ('class' in options) {
+      options['className'] = options['class'];
+      delete options['class'];
+    }
+    if ('for' in options) {
+      options['htmlFor'] = options['for'];
+      delete options['for'];
     }
     if (typeof options['className'] === 'object') {
       var names = []
@@ -218,10 +228,22 @@
     },
     init: function() {},
     refresh: function() {
-      var newTree = this.render();
-      var patches = virtualDom.diff(this.tree, newTree);
+      var newTree, patches;
+
+      serializeForms(this);
+      newTree = this.render();
+      patches = virtualDom.diff(this.tree, newTree);
       this.rootNode = virtualDom.patch(this.rootNode, patches);
       this.tree = newTree;
+    },
+    getValue: function(formName, attrName) {
+      var form = this.forms[formName];
+      if (form) return form[attrName];
+      else return {};
+    },
+    setValue: function(formName, attrName, value) {
+      if (!this.virtualForms[formName]) this.virtualForms[formName] = {};
+      this.virtualForms[formName][attrName] = value;
     }
   });
 
@@ -238,6 +260,8 @@
         component.forms[$(this).attr('name')] = obj;
       }
     });
+    $.extend(true, component.forms, component.virtualForms);
+    component.virtualForms = {};
   }
 
   if (!global.CapeJS) {
