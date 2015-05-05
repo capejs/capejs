1919584074a80daf91080e25af1cb6266aeff638
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-05-06 04:34:23 +0900

    Remove karma-coverage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,21 +1,14 @@
 module.exports = function(config) {
   config.set({
-      basePath: '',
-      autoWatch: true,
-      frameworks: ['mocha'],
-      plugins: [
-          'karma-mocha',
-          'karma-coverage',
-          'karma-chrome-launcher'
-      ],
-      browsers: ['Chrome'],
-      reporters: ['progress', 'coverage'],
-      preprocessors: {
-          '../lib/cape/*.js': ['coverage']
-      },
-      coverageReporter: {
-          dir: '../coverage/'
-      },
-      singleRun: true
+    basePath: '',
+    autoWatch: true,
+    frameworks: ['mocha'],
+    plugins: [
+      'karma-mocha',
+      'karma-chrome-launcher'
+    ],
+    browsers: ['Chrome'],
+    reporters: ['progress'],
+    singleRun: true
   })
 }
