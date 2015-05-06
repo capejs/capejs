d7e9a8d09d2a3605083e2c5ace4052567ea09f92
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-05-06 21:14:47 +0900

    Refine CSS of demo apps

--- demo/mixins/form_controls.js
+++ demo/mixins/form_controls.js
@@ -1,22 +1,22 @@
 var FormControls = {
   renderTextField: function(markup, fieldName, labelText) {
     markup.div({ class: 'form-group' }, function(m) {
-      m.labelFor(fieldName, labelText).sp();
+      m.labelFor(fieldName, labelText);
       m.textField(fieldName, { class: 'form-control' });
     })
   },
 
   renderTextareaField: function(markup, fieldName, labelText) {
     markup.div({ class: 'form-group' }, function(m) {
-      m.labelFor(fieldName, labelText).sp();
+      m.labelFor(fieldName, labelText);
       m.textareaField(fieldName, { class: 'form-control' });
     })
   },
 
   renderButtons: function(markup) {
     markup.div({ class: 'form-group' }, function(m) {
-      m.button('Submit', { class: 'btn btn-primary' }).sp();
-      m.button('Cancel', { class: 'btn btn-default' })
+      m.btn('Submit', { class: 'btn btn-primary' });
+      m.btn('Cancel', { class: 'btn btn-default' });
     })
   }
 }
