84a612a5cc974eef1b78ba7fcba4bc1b5a10ecd9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-05-19 01:28:44 +0900

    Update gulpfile.js
    [ci skip]

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var run = require('gulp-run');
 
 gulp.task('push', function() {
-  return run('hugo; cd ../gh-pages; git add .; git ci -m "automatic update"; git push').exec()
+  return run('hugo; cd ../gh-pages; git add .; git ci -m "automatic update [ci skip]"; git push').exec()
 });
