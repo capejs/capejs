d9dfe8f40bf6375a99689cd758a373900b0e8dc9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-07-08 23:42:31 +0900

    Implement ResourceAgent#destroy

--- test/spec/resource_agent_test.js
+++ test/spec/resource_agent_test.js
@@ -109,9 +109,9 @@ describe('ResourceAgent', function() {
       form = { id: 123, paramsFor: function() { return {} } };
       agent = new UserAgent(form);
 
-      spy1 = sinon.spy()
-      spy2 = sinon.spy()
-      spy3 = sinon.spy()
+      spy1 = sinon.spy();
+      spy2 = sinon.spy();
+      spy3 = sinon.spy();
       sinon.stub(global, 'fetch', function(path, options) {
         return {
           then: function(callback1) {
@@ -149,9 +149,9 @@ describe('ResourceAgent', function() {
       form = { id: 123, paramsFor: function() { return {} } };
       agent = new UserAgent(form);
 
-      spy1 = sinon.spy()
-      spy2 = sinon.spy()
-      spy3 = sinon.spy()
+      spy1 = sinon.spy();
+      spy2 = sinon.spy();
+      spy3 = sinon.spy();
       sinon.stub(global, 'fetch', function(path, options) {
         return {
           then: function(callback1) {
@@ -169,6 +169,46 @@ describe('ResourceAgent', function() {
       });
 
       agent.update(spy3);
+      expect(spy1.called).to.be.true;
+      expect(spy2.called).to.be.true;
+      expect(spy3.called).to.be.true;
+
+      global.fetch.restore();
+    })
+  })
+
+  describe('#destroy', function() {
+    it('should go through a fetch api chain', function() {
+      var UserAgent, form, agent, spy1, spy2, spy3;
+
+      UserAgent = function UserAgent(form) {
+        Cape.ResourceAgent.apply(this, arguments);
+      }
+      Cape.extend(UserAgent.prototype, Cape.ResourceAgent.prototype);
+
+      form = { id: 123 };
+      agent = new UserAgent(form);
+
+      spy1 = sinon.spy();
+      spy2 = sinon.spy();
+      spy3 = sinon.spy();
+      sinon.stub(global, 'fetch', function(path, options) {
+        return {
+          then: function(callback1) {
+            callback1.call(this, { json: spy1 })
+            return {
+              then: function(callback2) {
+                callback2.call(this, {})
+                return {
+                  catch: spy2
+                }
+              }
+            }
+          }
+        }
+      });
+
+      agent.destroy(spy3);
       expect(spy1.called).to.be.true;
       expect(spy2.called).to.be.true;
       expect(spy3.called).to.be.true;
