d3d7313ffa2da017c340ddcd6bb0eba0cdc3907c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-08-24 22:50:47 +0900

    CollectionAgent() should take single client

--- test/spec/collection_agent_test.js
+++ test/spec/collection_agent_test.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var UserCollectionAgent = Cape.createCollectionAgentClass({
-  constructor: function(options) {
+  constructor: function(client, options) {
     this.init(options);
+    this.client = client;
     this.resourceName = 'users';
     this.adapter = 'foo_bar';
   }
@@ -11,133 +12,14 @@ var UserCollectionAgent = Cape.createCollectionAgentClass({
 describe('CollectionAgent', function() {
   describe('constructor', function() {
     it('should take an object (options) as the second argument', function() {
-      var options, agent;
+      var component, options, agent;
 
+      component = {};
       options = { resourceName: 'users', basePath: '/api/' };
-      agent = new Cape.CollectionAgent(options);
+      agent = new Cape.CollectionAgent(component, options);
 
       expect(agent.resourceName).to.equal('users');
       expect(agent.basePath).to.equal('/api/');
-    })
-  })
-
-  describe('.getInstance', function() {
-    it('should create an instance of CollectionAgent', function() {
-      var agent;
-
-      agent = Cape.CollectionAgent.getInstance({ resourceName: 'users' });
-
-      expect(agent.resourceName).to.equal('users');
-    })
-
-    it('should return an existing instance of CollectionAgent', function() {
-      var agent1, agent2;
-
-      agent1 = Cape.CollectionAgent.getInstance({ resourceName: 'users' });
-      agent2 = Cape.CollectionAgent.getInstance({ resourceName: 'users' });
-
-      expect(agent1).to.equal(agent2);
-    })
-
-    it('should hold different instances by resource name', function() {
-      var agent1, agent2;
-
-      agent1 = Cape.CollectionAgent.getInstance({ resourceName: 'users' });
-      agent2 = Cape.CollectionAgent.getInstance({ resourceName: 'groups' });
-
-      expect(agent1).not.to.equal(agent2);
-    })
-
-    it('should hold different instances by basePath option', function() {
-      var agent1, agent2;
-
-      agent1 = Cape.CollectionAgent.getInstance(
-        { resourceName: 'users', basePath: '/foo/' });
-      agent2 = Cape.CollectionAgent.getInstance(
-        { resourceName: 'users', basePath: '/bar/' });
-
-      expect(agent1).not.to.equal(agent2);
-    })
-
-    it('should hold different instances by class', function() {
-      var agent1, agent2;
-
-      agent1 = Cape.CollectionAgent.getInstance({ resourceName: 'users' });
-      agent2 = UserCollectionAgent.getInstance({ resourceName: 'users' });
-
-      expect(agent1).not.to.equal(agent2);
-    })
-
-    it('should return an existing instance of UserCollectionAgent', function() {
-      var agent1, agent2;
-
-      agent1 = UserCollectionAgent.getInstance();
-      agent2 = UserCollectionAgent.getInstance();
-
-      expect(agent1).to.equal(agent2);
-    })
-
-    it('should return difference instances of UserCollectionAgent', function() {
-      var agent1, agent2;
-
-      agent1 = UserCollectionAgent.getInstance(
-        { basePath: '/foo/', nestedIn: 'groups/123' });
-      agent2 = UserCollectionAgent.getInstance(
-        { basePath: '/foo/', nestedIn: 'groups/456' });
-
-      expect(agent1).not.to.equal(agent2);
-      expect(agent1.basePath).to.equal('/foo/');
-      expect(agent2.basePath).to.equal('/foo/');
-      expect(agent1.nestedIn).to.equal('groups/123');
-      expect(agent2.nestedIn).to.equal('groups/456');
-    })
-  })
-
-  describe('attach', function() {
-    it('should register the given object as a target of propagation', function() {
-      var agent, component1, component2;
-
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
-
-      component1 = { refresh: sinon.spy() };
-      component2 = { refresh: sinon.spy() };
-
-      agent.attach(component1);
-      agent.attach(component2);
-      agent.propagate();
-      expect(component1.refresh.called).to.be.true;
-      expect(component2.refresh.called).to.be.true;
-    })
-
-    it('should not register the same object twice', function() {
-      var Klass, agent, component;
-
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
-
-      component = { refresh: sinon.spy() }
-
-      agent.attach(component);
-      expect(agent._.components.length).to.eq(1);
-    })
-  })
-
-  describe('detach', function() {
-    it('should unregister the given object as a target of propagation', function() {
-      var component1, component2, component3, agent;
-
-      component1 = { refresh: sinon.spy() }
-      component2 = { refresh: sinon.spy() }
-      component3 = { refresh: sinon.spy() }
-
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
-      agent.attach(component1);
-      agent.attach(component2);
-      agent.attach(component3);
-      agent.detach(component2);
-      agent.propagate();
-      expect(component1.refresh.called).to.be.true;
-      expect(component2.refresh.called).not.to.be.true;
-      expect(component3.refresh.called).to.be.true;
     })
   })
 
@@ -145,7 +27,7 @@ describe('CollectionAgent', function() {
     it('should return standard values', function() {
       var agent;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
 
       expect(agent.collectionPath()).to.equal('/users');
     })
@@ -153,7 +35,7 @@ describe('CollectionAgent', function() {
     it('should add prefix to the paths', function() {
       var agent;
 
-      agent = new Cape.CollectionAgent(
+      agent = new Cape.CollectionAgent({},
         { resourceName: 'users', basePath: '/api/' });
 
       expect(agent.collectionPath()).to.equal('/api/users');
@@ -162,7 +44,7 @@ describe('CollectionAgent', function() {
     it('should insert the "nestedIn" string to the paths', function() {
       var agent;
 
-      agent = new UserCollectionAgent(
+      agent = new UserCollectionAgent({},
         { basePath: '/api/', nestedIn: 'groups/123/' });
 
       expect(agent.collectionPath()).to.equal('/api/groups/123/users');
@@ -173,7 +55,7 @@ describe('CollectionAgent', function() {
     it('should return standard values', function() {
       var agent;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
 
       expect(agent.memberPath(123)).to.equal('/users/123');
     })
@@ -181,7 +63,8 @@ describe('CollectionAgent', function() {
     it('should add prefix to the paths', function() {
       var agent;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users', basePath: '/api/' });
+      agent = new Cape.CollectionAgent({},
+        { resourceName: 'users', basePath: '/api/' });
 
       expect(agent.memberPath(123)).to.equal('/api/users/123');
     })
@@ -196,7 +79,7 @@ describe('CollectionAgent', function() {
       var agent, spy1, spy2, spy3;
 
       Cape.AgentAdapters.FooBarAdapter = sinon.spy();
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       agent.adapter = 'foo_bar';
       sinon.stub(agent, 'refresh');
 
@@ -218,7 +101,7 @@ describe('CollectionAgent', function() {
     it('should accept text/plain data', function() {
       var agent, spy1, spy2, spy3;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users', dataType: 'text' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users', dataType: 'text' });
       sinon.stub(agent, 'refresh');
 
       spy1 = sinon.spy();
@@ -242,7 +125,7 @@ describe('CollectionAgent', function() {
     it('should go through a fetch api chain', function() {
       var agent, spy1, spy2, spy3;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'refresh');
 
       spy1 = sinon.spy();
@@ -261,7 +144,8 @@ describe('CollectionAgent', function() {
     it('should not call agent.refresh()', function() {
       var agent, spy1, spy2, spy3;
 
-      agent = new Cape.CollectionAgent({ resourceName: 'users', autoRefresh: false });
+      agent = new Cape.CollectionAgent({},
+        { resourceName: 'users', autoRefresh: false });
       sinon.stub(agent, 'refresh');
 
       spy1 = sinon.spy();
@@ -283,11 +167,12 @@ describe('CollectionAgent', function() {
     })
 
     it('should go through a fetch api chain', function() {
-      var agent, spy1;
+      var component, agent, spy1;
 
+      component = { refresh: function() {} };
       spy1 = sinon.spy();
       stubFetchAPI(spy1, { users: [ {}, {} ] });
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
       sinon.stub(agent, 'defaultErrorHandler');
       agent.refresh();
       expect(spy1.called).to.be.true;
@@ -296,10 +181,11 @@ describe('CollectionAgent', function() {
     })
 
     it('should call paramsForRefresh() to construct the request path', function() {
-      var agent, spy1;
+      var component, agent, spy1;
 
+      component = { refresh: function() {} };
       spy1 = stubFetchAPI(sinon.spy(), { users: [ {}, {} ], user_count: 2 });
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
       sinon.stub(agent, 'paramsForRefresh', function() { return { page: 1 } });
       sinon.stub(agent, 'defaultErrorHandler');
 
@@ -313,7 +199,7 @@ describe('CollectionAgent', function() {
 
       spy1 = sinon.spy();
       stubFetchAPI(spy1, { users: [ {}, {} ], user_count: 2 });
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'afterRefresh', function() {
         this.userCount = this.data.user_count;
       })
@@ -325,6 +211,22 @@ describe('CollectionAgent', function() {
       expect(agent.objects.length).to.equal(2);
       expect(agent.data.users.length).to.equal(2);
       expect(agent.userCount).to.equal(2);
+    })
+
+    it('should call the client.refresh()', function() {
+      var component, agent, spy1, spy2;
+
+      spy1 = sinon.spy();
+      spy2 = sinon.spy();
+      component = { refresh: spy1 };
+      stubFetchAPI(spy2, { users: [ {}, {} ], user_count: 2 });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
+      sinon.stub(agent, 'defaultErrorHandler');
+
+      agent.refresh();
+
+      expect(spy1.called).to.be.true;
+      expect(spy2.called).to.be.true;
     })
   })
 
@@ -338,7 +240,7 @@ describe('CollectionAgent', function() {
 
       spy = sinon.spy();
       stubFetchAPI(spy);
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'defaultErrorHandler');
       agent.index({ page: 1, per_page: 20 });
       expect(spy.called).to.be.true;
@@ -351,11 +253,12 @@ describe('CollectionAgent', function() {
     })
 
     it('should go through a fetch api chain', function() {
-      var agent, spy;
+      var component, agent, spy;
 
+      component = { refresh: function() {} };
       spy = sinon.spy();
       stubFetchAPI(spy);
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
       sinon.stub(agent, 'defaultErrorHandler');
       agent.create({ name: 'X', password: 'Y' });
       expect(spy.called).to.be.true;
@@ -368,11 +271,12 @@ describe('CollectionAgent', function() {
     })
 
     it('should go through a fetch api chain', function() {
-      var agent, spy;
+      var component, agent, spy;
 
+      component = { refresh: function() {} };
       spy = sinon.spy();
       stubFetchAPI(spy);
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
       sinon.stub(agent, 'defaultErrorHandler');
       agent.update(1, { name: 'X', password: 'Y' });
       expect(spy.called).to.be.true;
@@ -385,11 +289,12 @@ describe('CollectionAgent', function() {
     })
 
     it('should go through a fetch api chain', function() {
-      var agent, spy;
+      var component, agent, spy;
 
+      component = { refresh: function() {} };
       spy = sinon.spy();
       stubFetchAPI(spy);
-      agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      agent = new Cape.CollectionAgent(component, { resourceName: 'users' });
       sinon.stub(agent, 'defaultErrorHandler');
       agent.destroy(1);
       expect(spy.called).to.be.true;
@@ -398,7 +303,7 @@ describe('CollectionAgent', function() {
 
   describe('#get', function() {
     it('should call this.ajax() with "GET"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.get('suspended', null);
       expect(agent.ajax.calledWith('GET', '/users/suspended')).to.be.true;
@@ -407,7 +312,7 @@ describe('CollectionAgent', function() {
 
   describe('#head', function() {
     it('should call this.ajax() with "HEAD"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.head('suspended', null);
       expect(agent.ajax.calledWith('HEAD', '/users/suspended')).to.be.true;
@@ -416,7 +321,7 @@ describe('CollectionAgent', function() {
 
   describe('#post', function() {
     it('should call this.ajax() with "POST"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.post('tags', 1, { tags: [ 'A', 'B' ] });
       expect(agent.ajax.calledWith('POST', '/users/1/tags')).to.be.true;
@@ -425,7 +330,7 @@ describe('CollectionAgent', function() {
 
   describe('#patch', function() {
     it('should call this.ajax() with "PATCH"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.patch('suspend', 1, { name: 'X', password: 'Y' });
       expect(agent.ajax.calledWith('PATCH', '/users/1/suspend')).to.be.true;
@@ -434,7 +339,7 @@ describe('CollectionAgent', function() {
 
   describe('#put', function() {
     it('should call this.ajax() with "PUT"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.put('suspend', 1, { name: 'X', password: 'Y' });
       expect(agent.ajax.calledWith('PUT', '/users/1/suspend')).to.be.true;
@@ -443,7 +348,7 @@ describe('CollectionAgent', function() {
 
   describe('#delete', function() {
     it('should call this.ajax() with "DELETE"', function() {
-      var agent = new Cape.CollectionAgent({ resourceName: 'users' });
+      var agent = new Cape.CollectionAgent({}, { resourceName: 'users' });
       sinon.stub(agent, 'ajax');
       agent.delete('tags', 1);
       expect(agent.ajax.calledWith('DELETE', '/users/1/tags')).to.be.true;
