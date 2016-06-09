ae9c72049abdd1b5ead6ffee2352f77940a960c9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-06-09 14:44:54 +0900

    Remove gulp-run, which is deprecated

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
-var run = require('gulp-run');
+var exec = require('child_process').exec;
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var Server = require('karma').Server;
 
-gulp.task('build', function() {
-  return run('browserify --standalone Cape lib/cape.js > dist/cape.js').exec()
+gulp.task('build', function(cb) {
+  exec('browserify --standalone Cape lib/cape.js > dist/cape.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  })
 });
 
 gulp.task('minify', function() {
@@ -21,22 +25,6 @@ gulp.task('minify', function() {
 gulp.task('watch', function() {
   gulp.watch('./lib/**/*.js', ['build']);
 });
-
-var testFiles = [
-  'node_modules/chai/chai.js',
-  'node_modules/sinon/pkg/sinon.js',
-  'node_modules/whatwg/fetch.js',
-  'dist/cape.js',
-  'test/chai.js',
-  'demo/**/*.js',
-  'es6-demo/hello_message/*.js',
-  'es6-demo/todo_list/*.js',
-  'es6-demo/partials/*.js',
-  'test/integration/*.js',
-  'test/integration/es6/*.js',
-  'test/helpers.js',
-  'test/spec/*.js'
-];
 
 gulp.task('test', function (done) {
   new Server({
