8762ec9ceafcfbdc5351b837aa8a3c12a2595940
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-06-25 23:43:58 +0900

    Remove "double click counters" demo with ES5 syntax

--- es5-demo/double_click_counters/click_counter_store.js
+++
@@ -1,9 +0,0 @@
-var ClickCounterStore = Cape.createDataStoreClass({
-  init: function() {
-    this.counter = 0;
-  },
-  increment: function() {
-    this.counter++;
-    this.propagate();
-  }
-});
