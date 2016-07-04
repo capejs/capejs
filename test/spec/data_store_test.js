c7c3bb8d859b1e93764b903a492d675019d76226
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-07-05 00:53:50 +0900

    Rewrite DataStore class with ES6 syntax

--- test/spec/data_store_test.js
+++ test/spec/data_store_test.js
@@ -1,100 +1,86 @@
-'use strict';
+'use strict'
 
-describe('DataStore', function() {
-  describe('constructor', function() {
-    it('should call "this.init()" if defined', function() {
-      var Klass, method;
+describe('DataStore', () => {
+  describe('constructor', () => {
+    it('should call "this.init()" if defined', () => {
+      let method = sinon.spy()
+      class Klass extends Cape.DataStore {
+        init() { method.call() }
+      }
 
-      method = sinon.spy();
-      Klass = Cape.createDataStoreClass({
-        init: method
-      })
-
-      new Klass();
-      expect(method.called).to.be.true;
+      new Klass()
+      expect(method.called).to.be.true
     })
   })
 
-  describe('.create', function() {
-    it('should always return the singleton object', function() {
-      var Klass, object1, object2;
+  describe('.create', () => {
+    it('should always return the singleton object', () => {
+      class Klass extends Cape.DataStore {}
 
-      Klass = Cape.createDataStoreClass({})
-
-      object1 = Klass.create();
-      object2 = Klass.create();
-      expect(object1).to.equal(object2);
+      let object1 = Klass.create()
+      let object2 = Klass.create()
+      expect(object1).to.equal(object2)
     })
 
-    it('should create an unique object', function() {
-      var Klass1, Klass2, object1, object2;
+    it('should create an unique object', () => {
+      class Klass1 extends Cape.DataStore {}
+      class Klass2 extends Cape.DataStore {}
 
-      Klass1 = Cape.createDataStoreClass({})
-      Klass2 = Cape.createDataStoreClass({})
-
-      object1 = Klass1.create();
-      object2 = Klass2.create();
-      expect(object1).not.to.equal(object2);
+      let object1 = Klass1.create()
+      let object2 = Klass2.create()
+      expect(object1).not.to.equal(object2)
     })
 
-    it('should accept options for the constructor', function() {
-      var Klass, object;
+    it('should accept options for the constructor', () => {
+      class Klass extends Cape.DataStore {}
 
-      Klass = Cape.createDataStoreClass({})
-
-      object = Klass.create({ x: 1, y: 2 });
-      expect(object.options.x).to.equal(1);
-      expect(object.options.y).to.equal(2);
+      let object = Klass.create({ x: 1, y: 2 })
+      expect(object.options.x).to.equal(1)
+      expect(object.options.y).to.equal(2)
     })
   })
 
-  describe('attach', function() {
-    it('should register the given object as a target of propagation', function() {
-      var Klass, component1, component2, ds;
+  describe('attach', () => {
+    it('should register the given object as a target of propagation', () => {
+      class Klass extends Cape.DataStore {}
+      let component1 = { refresh: sinon.spy() }
+      let component2 = { refresh: sinon.spy() }
 
-      Klass = Cape.createDataStoreClass({});
-      component1 = { refresh: sinon.spy() };
-      component2 = { refresh: sinon.spy() };
-
-      ds = new Klass();
-      ds.attach(component1);
-      ds.attach(component2);
-      ds.propagate();
-      expect(component1.refresh.called).to.be.true;
-      expect(component2.refresh.called).to.be.true;
+      let ds = new Klass()
+      ds.attach(component1)
+      ds.attach(component2)
+      ds.propagate()
+      expect(component1.refresh.called).to.be.true
+      expect(component2.refresh.called).to.be.true
     })
 
-    it('should not register the same object twice', function() {
-      var Klass, component, ds;
+    it('should not register the same object twice', () => {
+      class Klass extends Cape.DataStore {}
+      let component = { refresh: sinon.spy() }
 
-      Klass = Cape.createDataStoreClass({});
-      component = { refresh: sinon.spy() }
-
-      ds = new Klass();
-      ds.attach(component);
-      ds.attach(component);
-      expect(ds._.components.length).to.eq(1);
+      let ds = new Klass()
+      ds.attach(component)
+      ds.attach(component)
+      expect(ds._.components.length).to.eq(1)
     })
   })
 
-  describe('detach', function() {
-    it('should unregister the given object as a target of propagation', function() {
-      var Klass, component1, component2, component3, ds;
+  describe('detach', () => {
+    it('should unregister the given object as a target of propagation', () => {
+      class Klass extends Cape.DataStore {}
+      let component1 = { refresh: sinon.spy() }
+      let component2 = { refresh: sinon.spy() }
+      let component3 = { refresh: sinon.spy() }
 
-      Klass = Cape.createDataStoreClass({});
-      component1 = { refresh: sinon.spy() }
-      component2 = { refresh: sinon.spy() }
-      component3 = { refresh: sinon.spy() }
-
-      ds = new Klass();
-      ds.attach(component1);
-      ds.attach(component2);
-      ds.attach(component3);
-      ds.detach(component2);
-      ds.propagate();
-      expect(component1.refresh.called).to.be.true;
-      expect(component2.refresh.called).not.to.be.true;
-      expect(component3.refresh.called).to.be.true;
+      let ds = new Klass()
+      ds.attach(component1)
+      ds.attach(component2)
+      ds.attach(component3)
+      ds.detach(component2)
+      ds.propagate()
+      expect(component1.refresh.called).to.be.true
+      expect(component2.refresh.called).not.to.be.true
+      expect(component3.refresh.called).to.be.true
     })
   })
 })
